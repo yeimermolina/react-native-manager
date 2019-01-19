0658687dc5c8c3efbe86d86d07a74e0ba08964d7
Yeimer Molina <yeimer.molina@gmail.com>
2019-01-19 16:55:14 -0500

    Add initial project setup

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDKJmm7SC-RgWP6UJ22dV9NbkqyYwLT1Vs',
+      authDomain: 'react-manager-799c0.firebaseapp.com',
+      databaseURL: 'https://react-manager-799c0.firebaseio.com',
+      projectId: 'react-manager-799c0',
+      storageBucket: 'react-manager-799c0.appspot.com',
+      messagingSenderId: '884250531097'
+    });
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello Yeimer!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
