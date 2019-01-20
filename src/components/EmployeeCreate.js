bd0cd83b4d263d4fbddac4029a8d85d2fe846abe
Yeimer Molina <yeimer.molina@gmail.com>
2019-01-20 12:00:25 -0500

    Minor update

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
 import { employeeUpdate } from '../actions';
@@ -26,28 +26,39 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
-        <CardSection>
-          <Picker
-            style={{flex: 1}}
-            selectedValue={this.props.shift}
-            onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day})}
-          >
-            <Picker.Item label="Monday" value="Monday" />
-            <Picker.Item label="Tuesday" value="Tuesday" />
-            <Picker.Item label="Wednesday" value="Wednesday" />
-            <Picker.Item label="Thursday" value="Thursday" />
-            <Picker.Item label="Friday" value="Friday" />
-            <Picker.Item label="Satuday" value="Satuday" />
-            <Picker.Item label="Sunday" value="Sunday" />
-          </Picker>
+        <CardSection style={{ flexDirection: 'column'}}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
+          <View>
+            <Picker
+              selectedValue={this.props.shift}
+              onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day})}
+            >
+              <Picker.Item label="Monday" value="Monday" />
+              <Picker.Item label="Tuesday" value="Tuesday" />
+              <Picker.Item label="Wednesday" value="Wednesday" />
+              <Picker.Item label="Thursday" value="Thursday" />
+              <Picker.Item label="Friday" value="Friday" />
+              <Picker.Item label="Satuday" value="Satuday" />
+              <Picker.Item label="Sunday" value="Sunday" />
+            </Picker>
+          </View>
         </CardSection>
+        
         <CardSection>
           <Button 
             buttonText="Create"
           />
         </CardSection>
+        
       </Card>
     )
+  }
+}
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
   }
 }
 
--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const CardSection = ({ children }) => (
-  <View style={styles.containerStyle}>
+const CardSection = ({ children, style }) => (
+  <View style={[styles.containerStyle, style]}>
     {children}
   </View>
 );
